a60d1d4285582b1cec35f64eef3e46e33464c1ca
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:32 -0500

    TUnit conversion

--- test/Soenneker.Radarr.OpenApiClient.Tests/RadarrOpenApiClientTests.cs
+++ test/Soenneker.Radarr.OpenApiClient.Tests/RadarrOpenApiClientTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Radarr.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class RadarrOpenApiClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class RadarrOpenApiClientTests : HostedUnitTest
 {
-    public RadarrOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public RadarrOpenApiClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
